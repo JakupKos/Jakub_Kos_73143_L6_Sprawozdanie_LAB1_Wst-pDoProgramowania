15a88b64f2ca6938128671353e093157fbff2907
Jakub <73143@student.wsiz.edu.pl>
2025-10-24 16:59:15 +0200

    Lab

---
+++ zad2.py
@@ -0,0 +1,8 @@
+#=====================================================
+# zad. 2
+uczelnia = "Studiuję na WSIiZ"
+print(uczelnia)
+
+"""
+do zmiennej "uczelnia" przypisany jest ciąg znaków, który następnie zostaje pokazany za pomocą funkcji print()
+"""
